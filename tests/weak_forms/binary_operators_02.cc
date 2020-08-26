e51b06e6fa3d2a32b4a41d615d599653da5f2c57
Jean-Paul Pelteret <jppelteret@gmail.com>
2020-08-26 22:19:54 +0200

    Finish a binary operator test

--- tests/weak_forms/binary_operators_02.cc
+++ tests/weak_forms/binary_operators_02.cc
@@ -96,206 +96,113 @@ run()
     std::cout << "Scalar + value: " << ((f1 + value).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
     std::cout << "Scalar - value: " << ((f1 - value).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
 
+    std::cout << "Scalar + Laplacian: " << ((f1 + laplacian).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "Scalar - Laplacian: " << ((f1 - laplacian).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+
     deallog << "OK" << std::endl;
   }
 
-  // {
-  //   const std::string title = "Scalar functor";
-  //   std::cout << title << std::endl;
-  //   deallog << title << std::endl;
+  {
+    const std::string title = "Vector";
+    std::cout << title << std::endl;
+    deallog << title << std::endl;
 
-  //   using namespace WeakForms;
+    using namespace WeakForms;
 
-  //   const ScalarFunctor c1("c1", "c1");
-  //   const auto f1 = value<double>(c1, [](const unsigned int) {
-  //     return 1.0;
-  //   });
+    const VectorFunctor<dim> v1("v1", "v1");
+    const auto f1 = value<double>(v1, [](const unsigned int) {
+      Tensor<1,dim> t;
+      for (auto it=t.begin_raw(); it!=t.end_raw(); ++it)
+        *it = 2.0;
+      return t;
+    });
 
-  //   const ScalarFunctor c2("c2", "c2");
-  //   const auto f2 = value<double>(c2, [](const unsigned int) {
-  //     return 2.0;
-  //   });
+    const FieldSolution<dim, spacedim> field_solution;
+    const auto value = field_solution.value();
+    const auto gradient = field_solution.gradient();
+    const auto laplacian = field_solution.laplacian();
+    const auto hessian = field_solution.hessian();
+    const auto third_derivative = field_solution.third_derivative();
 
-  //   const ScalarFunctor c3("c3", "c3");
-  //   const auto f3 = value<double>(c3, [](const unsigned int) {
-  //     return 3.0;
-  //   });
+    std::cout << "Vector * value: " << ((f1 * value).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "Vector * gradient: " << ((f1 * gradient).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "Vector * Laplacian: " << ((f1 * laplacian).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "Vector * Hessian: " << ((f1 * hessian).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "Vector * third derivative: " << ((f1 * third_derivative).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
 
-  //   const ScalarFunctor c4("c4", "c4");
-  //   const auto f4 = value<double>(c4, [](const unsigned int) {
-  //     return 4.0;
-  //   });
+    std::cout << "Vector + gradient: " << ((f1 + gradient).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "Vector - gradient: " << ((f1 - gradient).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
 
-  //   std::cout << "Addition 1: " << ((f1+f2).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Addition 2: " << ((f1+(f2+f3)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Addition 3: " << (((f1+f2)+f3).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Addition 4: " << (((f1+f2)+(f3+f4)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
+    deallog << "OK" << std::endl;
+  }
 
-  //   std::cout << "Subtraction 1: " << ((f1-f2).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Subtraction 2: " << ((f1-(f2-f3)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Subtraction 3: " << (((f1-f2)-f3).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Subtraction 4: " << (((f1-f2)-(f3-f4)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
+  {
+    const std::string title = "Tensor";
+    std::cout << title << std::endl;
+    deallog << title << std::endl;
 
-  //   std::cout << "Multiplication 1: " << ((f1*f2).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Multiplication 2: " << ((f1*(f2*f3)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Multiplication 3: " << (((f1*f2)*f3).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Multiplication 4: " << (((f1*f2)*(f3*f4)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
+    using namespace WeakForms;
 
-  //   deallog << "OK" << std::endl;
-  // }
+    const TensorFunctor<2,dim> T1("T1", "T1");
+    const auto f1 = value<double>(T1, [](const unsigned int) {
+      Tensor<2,dim> t;
+      for (auto it=t.begin_raw(); it!=t.end_raw(); ++it)
+        *it = 2.0;
+      return t;
+    });
 
-  // {
-  //   const std::string title = "Tensor functor";
-  //   std::cout << title << std::endl;
-  //   deallog << title << std::endl;
+    const FieldSolution<dim, spacedim> field_solution;
+    const auto value = field_solution.value();
+    const auto gradient = field_solution.gradient();
+    const auto laplacian = field_solution.laplacian();
+    const auto hessian = field_solution.hessian();
+    const auto third_derivative = field_solution.third_derivative();
 
-  //   using namespace WeakForms;
+    std::cout << "Tensor * value: " << ((f1 * value).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "Tensor * gradient: " << ((f1 * gradient).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "Tensor * Laplacian: " << ((f1 * laplacian).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "Tensor * Hessian: " << ((f1 * hessian).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "Tensor * third derivative: " << ((f1 * third_derivative).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
 
-  //   const TensorFunctor<2, spacedim>   t1("C1", "C1");
-  //   const auto tf1 = value<double>(t1, [](const unsigned int) {
-  //     return Tensor<2, dim, double>(unit_symmetric_tensor<spacedim>());
-  //   });
+    std::cout << "Tensor + Hessian: " << ((f1 + hessian).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "Tensor - Hessian: " << ((f1 - hessian).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
 
-  //   const TensorFunctor<2, spacedim>   t2("C1", "C1");
-  //   const auto tf2 = value<double>(t2, [](const unsigned int) {
-  //     return Tensor<2, dim, double>(2.0*unit_symmetric_tensor<spacedim>());
-  //   });
+    deallog << "OK" << std::endl;
+  }
 
-  //   const TensorFunctor<2, spacedim>   t3("C3", "C3");
-  //   const auto tf3 = value<double>(t3, [](const unsigned int) {
-  //     return Tensor<2, dim, double>(3.0*unit_symmetric_tensor<spacedim>());
-  //   });
+  {
+    const std::string title = "SymmetricTensor";
+    std::cout << title << std::endl;
+    deallog << title << std::endl;
 
-  //   const TensorFunctor<2, spacedim>   t4("C4", "C4");
-  //   const auto tf4 = value<double>(t4, [](const unsigned int) {
-  //     return Tensor<2, dim, double>(4.0*unit_symmetric_tensor<spacedim>());
-  //   });
+    using namespace WeakForms;
 
-  //   const ScalarFunctor c1("c1", "c1");
-  //   const auto f1 = value<double>(c1, [](const unsigned int) {
-  //     return 1.0;
-  //   });
+    const SymmetricTensorFunctor<2,dim> S1("S1", "S1");
+    const auto f1 = value<double>(S1, [](const unsigned int) {
+      SymmetricTensor<2,dim> t;
+      for (auto it=t.begin_raw(); it!=t.end_raw(); ++it)
+        *it = 2.0;
+      return t;
+    });
 
-  //   const ScalarFunctor c2("c2", "c2");
-  //   const auto f2 = value<double>(c2, [](const unsigned int) {
-  //     return 2.0;
-  //   });
+    const FieldSolution<dim, spacedim> field_solution;
+    const auto value = field_solution.value();
+    const auto gradient = field_solution.gradient();
+    const auto laplacian = field_solution.laplacian();
+    const auto hessian = field_solution.hessian();
+    const auto third_derivative = field_solution.third_derivative();
 
-  //   std::cout << "Addition 1: " << ((tf1+tf2).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Addition 2: " << ((tf1+(tf2+tf3)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Addition 3: " << (((tf1+tf2)+tf3).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Addition 4: " << (((tf1+tf2)+(tf3+tf4)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Addition 5: " << (((f1*tf1)+(tf2*f2)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
+    std::cout << "SymmetricTensor * value: " << ((f1 * value).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "SymmetricTensor * gradient: " << ((f1 * gradient).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "SymmetricTensor * Laplacian: " << ((f1 * laplacian).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "SymmetricTensor * Hessian: " << ((f1 * hessian).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "SymmetricTensor * third derivative: " << ((f1 * third_derivative).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
 
-  //   std::cout << "Subtraction 1: " << ((tf1-tf2).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Subtraction 2: " << ((tf1-(tf2-tf3)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Subtraction 3: " << (((tf1-tf2)-tf3).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Subtraction 4: " << (((tf1-tf2)-(tf3-tf4)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Subtraction 5: " << (((f1*tf1)-(tf2*f2)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
+    std::cout << "SymmetricTensor + Hessian: " << ((f1 + hessian).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
+    std::cout << "SymmetricTensor - Hessian: " << ((f1 - hessian).template operator()<NumberType> (fe_values,solution))[q_point] << std::endl;
 
-  //   std::cout << "Single contraction 1: " << ((tf1*tf2).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Single contraction 2: " << ((tf1*(tf2*tf3)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Single contraction 3: " << (((tf1*tf2)*tf3).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Single contraction 4: " << (((tf1*tf2)*(tf3*tf4)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Single contraction 5: " << (((f1*tf1)*(tf2*f2)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-
-  //   deallog << "OK" << std::endl;
-  // }
-
-  // {
-  //   const std::string title = "Scalar function functor";
-  //   std::cout << title << std::endl;
-  //   deallog << title << std::endl;
-
-  //   using namespace WeakForms;
-
-  //   const ScalarFunctionFunctor<spacedim> c1("c1", "c1");
-  //   const ConstantFunction<spacedim, double> constant_scalar_function_1(1.0);
-  //   const auto f1 = value<double>(c1, constant_scalar_function_1);
-
-  //   const ScalarFunctionFunctor<spacedim> c2("c2", "c2");
-  //   const ConstantFunction<spacedim, double> constant_scalar_function_2(2.0);
-  //   const auto f2 = value<double>(c2, constant_scalar_function_2);
-
-  //   const ScalarFunctionFunctor<spacedim> c3("c3", "c3");
-  //   const ConstantFunction<spacedim, double> constant_scalar_function_3(3.0);
-  //   const auto f3 = value<double>(c3, constant_scalar_function_3);
-
-  //   const ScalarFunctionFunctor<spacedim> c4("c4", "c4");
-  //   const ConstantFunction<spacedim, double> constant_scalar_function_4(4.0);
-  //   const auto f4 = value<double>(c4, constant_scalar_function_4);
-
-  //   std::cout << "Addition 1: " << ((f1+f2).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Addition 2: " << ((f1+(f2+f3)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Addition 3: " << (((f1+f2)+f3).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Addition 4: " << (((f1+f2)+(f3+f4)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-
-  //   std::cout << "Subtraction 1: " << ((f1-f2).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Subtraction 2: " << ((f1-(f2-f3)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Subtraction 3: " << (((f1-f2)-f3).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Subtraction 4: " << (((f1-f2)-(f3-f4)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-
-  //   std::cout << "Multiplication 1: " << ((f1*f2).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Multiplication 2: " << ((f1*(f2*f3)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Multiplication 3: " << (((f1*f2)*f3).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Multiplication 4: " << (((f1*f2)*(f3*f4)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-
-  //   deallog << "OK" << std::endl;
-  // }
-
-  // {
-  //   const std::string title = "Tensor function functor";
-  //   std::cout << title << std::endl;
-  //   deallog << title << std::endl;
-
-  //   using namespace WeakForms;
-
-  //   const TensorFunctionFunctor<2, spacedim>   t1("C1", "C1");
-  //   const ConstantTensorFunction<2, dim, double> constant_tensor_function_1(unit_symmetric_tensor<dim>());
-  //   const auto tf1 = value(t1, constant_tensor_function_1);
-
-  //   const TensorFunctionFunctor<2, spacedim>   t2("C1", "C1");
-  //   const ConstantTensorFunction<2, dim, double> constant_tensor_function_2(2.0*unit_symmetric_tensor<dim>());
-  //   const auto tf2 = value(t2, constant_tensor_function_2);
-
-  //   const TensorFunctionFunctor<2, spacedim>   t3("C3", "C3");
-  //   const ConstantTensorFunction<2, dim, double> constant_tensor_function_3(3.0*unit_symmetric_tensor<dim>());
-  //   const auto tf3 = value(t3, constant_tensor_function_3);
-
-  //   const TensorFunctionFunctor<2, spacedim>   t4("C4", "C4");
-  //   const ConstantTensorFunction<2, dim, double> constant_tensor_function_4(4.0*unit_symmetric_tensor<dim>());
-  //   const auto tf4 = value(t4, constant_tensor_function_4);
-
-  //   const ScalarFunctor c1("c1", "c1");
-  //   const auto f1 = value<double>(c1, [](const unsigned int) {
-  //     return 1.0;
-  //   });
-
-  //   const ScalarFunctor c2("c2", "c2");
-  //   const auto f2 = value<double>(c2, [](const unsigned int) {
-  //     return 2.0;
-  //   });
-
-  //   std::cout << "Addition 1: " << ((tf1+tf2).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Addition 2: " << ((tf1+(tf2+tf3)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Addition 3: " << (((tf1+tf2)+tf3).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Addition 4: " << (((tf1+tf2)+(tf3+tf4)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Addition 5: " << (((f1*tf1)+(tf2*f2)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-
-  //   std::cout << "Subtraction 1: " << ((tf1-tf2).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Subtraction 2: " << ((tf1-(tf2-tf3)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Subtraction 3: " << (((tf1-tf2)-tf3).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Subtraction 4: " << (((tf1-tf2)-(tf3-tf4)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Subtraction 5: " << (((f1*tf1)-(tf2*f2)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-
-  //   std::cout << "Single contraction 1: " << ((tf1*tf2).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Single contraction 2: " << ((tf1*(tf2*tf3)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Single contraction 3: " << (((tf1*tf2)*tf3).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Single contraction 4: " << (((tf1*tf2)*(tf3*tf4)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-  //   std::cout << "Single contraction 5: " << (((f1*tf1)*(tf2*f2)).template operator()<NumberType> (fe_values))[q_point] << std::endl;
-
-  //   deallog << "OK" << std::endl;
-  // }
+    deallog << "OK" << std::endl;
+  }
 
   deallog << "OK" << std::endl;
 }
